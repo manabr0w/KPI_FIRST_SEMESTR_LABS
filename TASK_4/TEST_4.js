d0a2d658aff8e0d541534db53f781f5475c3da7e
maksim <maksimfedorenko71@gmail.com>
2024-12-22 19:01:12 +0200

    created test

--- TASK_4/TEST_4.js
+++ TASK_4/TEST_4.js
@@ -1,3 +1,5 @@
+const asyncFilter = require('./asyncFilter');
+
 async function asyncCallback(item, callback) {
     setTimeout(() => {
         if (item % 5 === 0) {
@@ -8,17 +10,30 @@ async function asyncCallback(item, callback) {
     }, Math.random() * 1000);
 }
 
-const largeDataset = Array.from({ length: 1000 }, (_, i) => i + 1);
+test('asyncFilter processes data correctly', async () => {
+    const dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const abortSignal = new AbortController();
-setTimeout(() => {
-    abortSignal.abort();
-}, 2000);
+    const result = await asyncFilter(dataset, asyncCallback);
 
-asyncFilter(largeDataset, asyncCallback, 500, abortSignal.signal)
-    .then((results) => {
-        console.log('Processed Results:', results);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+    expect(result).toEqual([1, 2, 3, 4, 6, 7, 8, 9]);
+});
+
+test('asyncFilter aborts processing correctly', async () => {
+    const dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const abortSignal = new AbortController();
+
+    setTimeout(() => abortSignal.abort(), 1000);
+
+    try {
+        await asyncFilter(dataset, asyncCallback, 500, abortSignal.signal);
+    } catch (error) {
+        expect(error.message).toBe('Operation aborted');
+    }
+});
+
+test('asyncFilter processes with debounce correctly', async () => {
+    const dataset = [1, 2, 3, 4, 5];
+    const result = await asyncFilter(dataset, asyncCallback, 500);
+
+    expect(result).toEqual([1, 2, 3, 4, 6]);
+});
