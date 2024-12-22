f68395b34e713e49862760ec3d535683eb7cf031
maksim <maksimfedorenko71@gmail.com>
2024-12-22 15:52:35 +0200

    added async callback

--- TASK_1/TASK_1.js
+++ TASK_1/TASK_1.js
@@ -4,16 +4,16 @@ function asyncFilter(arr, asyncCallback, finalCallback) {
     let completed = 0;
 
     arr.forEach((item, index) => {
-        asyncCallback(item, (err, keep) => {
-            if (err) return finalCallback(err);
+        const start = Date.now();
 
-            if (keep) results.push(item);
-            completed++;
-            if (completed === arr.length) {
-                finalCallback(null, results);
-            }
-        });
-    });
+    asyncCallback(item, (err, keep) => {
+        if (err) return finalCallback(err);
+
+        const elapsed = Date.now() - start;
+        const delay = debounceTime - elapsed > 0 ? debounceTime - elapsed : 0;
+
+
+    })
 }
 
 
