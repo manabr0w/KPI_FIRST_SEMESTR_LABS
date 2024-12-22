92885aea9a83a5ba7ca6d29be363587244a6a7aa
maksim <maksimfedorenko71@gmail.com>
2024-12-22 15:59:12 +0200

    added err check funcionality

--- TASK_1/TASK_1.js
+++ TASK_1/TASK_1.js
@@ -2,12 +2,18 @@
 function asyncFilter(arr, asyncCallback, finalCallback) {
     let results = [];
     let completed = 0;
+    let errorOcurred = false;
 
     arr.forEach((item, index) => {
+        if (errorOcurred) return;
+
         const start = Date.now();
 
     asyncCallback(item, (err, keep) => {
-        if (err) return finalCallback(err);
+        if (err) {
+            errorOcurred = true;
+            return finalCallback(err);
+        }
 
         const elapsed = Date.now() - start;
         const delay = debounceTime - elapsed > 0 ? debounceTime - elapsed : 0;
