920e3327a9faba5421f9bf556fb69d485a6c9c2f
maksim <maksimfedorenko71@gmail.com>
2024-12-22 15:38:14 +0200

    added check for elements

--- TASK_1/TASK_1.js
+++ TASK_1/TASK_1.js
@@ -6,3 +6,12 @@ function asyncFilter(arr, asyncCallback, finalCallback) {
     arr.forEach((item, index) => {
         asyncCallback(item, (err, keep) => {
             if (err) return finalCallback(err);
+
+            if (keep) results.push(item);
+            completed++;
+            if (completed === arr.length) {
+                finalCallback(null, results);
+            }
+        });
+    });
+}
