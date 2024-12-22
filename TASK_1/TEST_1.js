20e6275233fdbc98724fd91bfa82a6ff6d5a1674
maksim <maksimfedorenko71@gmail.com>
2024-12-22 15:56:23 +0200

    updated test for bounce

--- TASK_1/TEST_1.js
+++ TASK_1/TEST_1.js
@@ -4,7 +4,7 @@ function asyncCheckIfEven(number, callback) {
     setTimeout(() => {
         if (number < 0) return callback(new Error("Negative numbers not allowed"));
         callback(null, number % 2 === 0);
-    }, Math.random() * 500);
+    }, Math.random() * 300);
 }
 
 const numbers = [1, 2, 3, 4, -1, 6];
@@ -16,7 +16,8 @@ asyncFilter(
         if (err) {
             console.error("Error:", err.message);
         } else {
-            console.log("Filtered Results:", results);
+            console.log("Filtered Results with Debounce:", results);
         }
-    }
+    },
+    500
 );
