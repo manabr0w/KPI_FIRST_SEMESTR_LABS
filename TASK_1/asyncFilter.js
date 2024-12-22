3702ecd6047b0f2b3e21a3bcef607fda0170df28
maksim <maksimfedorenko71@gmail.com>
2024-12-22 16:21:33 +0200

    updated asyncDilter.js for saving order

--- TASK_1/asyncFilter.js
+++ TASK_1/asyncFilter.js
@@ -1,27 +1,30 @@
 function asyncFilter(arr, asyncCallback, finalCallback, debounceTime = 0) {
-    let results = [];
+    let results = new Array(arr.length);
     let completed = 0;
-    let errorOcurred = false;
+    let errorOccurred = false;
 
     arr.forEach((item, index) => {
-        if (errorOcurred) return;
+        if (errorOccurred) return;
 
         const start = Date.now();
 
         asyncCallback(item, (err, keep) => {
             if (err) {
-                errorOcurred = true;
-                return finalCallback(err);
+                if (!errorOccurred) {
+                    errorOccurred = true;
+                    return finalCallback(err);
+                }
             }
 
             const elapsed = Date.now() - start;
             const delay = debounceTime - elapsed > 0 ? debounceTime - elapsed : 0;
 
             setTimeout(() => {
-                if (keep) results.push(item);
+                if (!errorOccurred && keep) results[index] = item;
                 completed++;
-                if (completed === arr.length) {
-                    finalCallback(null, results);
+
+                if (completed === arr.length && !errorOccurred) {
+                    finalCallback(null, results.filter(x => x !== undefined));
                 }
             }, delay);
         });
