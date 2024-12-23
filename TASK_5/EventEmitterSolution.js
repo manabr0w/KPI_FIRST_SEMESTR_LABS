e28b35fd431bc80a0930c9c34a03d0fb5b846780
maksim <maksimfedorenko71@gmail.com>
2024-12-23 13:29:07 +0200

    Added support for cancellable operations in MessageBus

--- TASK_5/EventEmitterSolution.js
+++ TASK_5/EventEmitterSolution.js
@@ -1,20 +1,33 @@
-const MessageBus = require('./MessageBus');
+const { EventEmitter } = require('events');
 
-class Entity {
-    constructor(name, messageBus) {
-        this.name = name;
-        this.messageBus = messageBus;
+class CancellableMessageBus extends EventEmitter {
+    constructor() {
+        super();
+        this.abortControllers = new Map();
     }
 
-    send(event, data) {
-        console.log(`[${this.name}] Sending event: ${event}`, data);
-        this.messageBus.sendMessage(event, data);
+    sendMessage(event, data, abortSignal) {
+        if (abortSignal?.aborted) {
+            console.log(`[CancellableMessageBus] Message aborted: ${event}`);
+            return;
+        }
+
+        this.emit(event, data);
     }
 
-    receive(event, callback) {
-        console.log(`[${this.name}] Listening for event: ${event}`);
-        this.messageBus.receiveMessage(event, callback);
+    cancelMessage(event) {
+        const abortController = this.abortControllers.get(event);
+        if (abortController) {
+            abortController.abort();
+            console.log(`[CancellableMessageBus] Cancelled event: ${event}`);
+        }
+    }
+
+    createAbortSignal(event) {
+        const abortController = new AbortController();
+        this.abortControllers.set(event, abortController);
+        return abortController.signal;
     }
 }
 
-module.exports = Entity;
+module.exports = CancellableMessageBus;
