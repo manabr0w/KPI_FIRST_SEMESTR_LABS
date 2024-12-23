c4b0967fdc3a60b4c3ebeb791f71a2c71f23944a
maksim <maksimfedorenko71@gmail.com>
2024-12-23 13:43:41 +0200

    added time complexity checker

--- TASK_5/TEST_5.js
+++ TASK_5/TEST_5.js
@@ -5,8 +5,11 @@ const messageBus = new CancellableMessageBus();
 const entityA = new Entity('EntityA', messageBus);
 const entityB = new Entity('EntityB', messageBus);
 
+console.time('Task Execution Time');
+
 entityB.receive('greet', (data) => {
     console.log(`[EntityB] Received:`, data);
+    console.timeEnd('Task Execution Time');
 });
 
 entityA.send('greet', { message: 'Hello from EntityA!' });
