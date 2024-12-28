ff4f9641761d36032cefb8e526bcfe8f320c755f
maksim <maksimfedorenko71@gmail.com>
2024-12-28 15:57:20 +0200

    updated test

--- TASK_3/TEST_3.js
+++ TASK_3/TEST_3.js
@@ -1,6 +1,4 @@
-const asyncFilter = require('./asyncFilter'); // Імпортуємо вашу функцію
-
-
+const asyncFilter = require('./asyncFilter');
 
 const asyncCheckEven = (value, signal) => {
     let timeout;
@@ -28,41 +26,40 @@ const asyncCheckEven = (value, signal) => {
 };
 
 (async () => {
-    console.log("\n=== Test 1: Фільтрація без скасування ===");
+    console.log("\n=== Test 1: Filtering without cancellation ===");
     const numbers1 = [1, 2, 5, 123, 3, 9, 0];
     try {
         const result1 = await asyncFilter(numbers1, asyncCheckEven, null, 100);
-        console.log("Результат фільтрації:", result1);
+        console.log("Filtering result:", result1);
     } catch (err) {
-        console.error("Помилка:", err.message);
-
+        console.error("Error:", err.message);
     }
 
-    console.log("\n=== Test 2: Скасування через AbortController ===");
+    console.log("\n=== Test 2: Cancellation using AbortController ===");
     const numbers2 = [1, 2, 5, 123, 3, 9, 0];
     const controller2 = new AbortController();
     const signal2 = controller2.signal;
     setTimeout(() => {
-        console.log("Абортую операції...");
+        console.log("Aborting operations...");
         controller2.abort();
     }, 2000);
     try {
         const result2 = await asyncFilter(numbers2, asyncCheckEven, signal2, 100);
-        console.log("Результат фільтрації:", result2);
+        console.log("Filtering result:", result2);
     } catch (err) {
         if (err.message === 'AbortError') {
-            console.error("Операції було скасовано.");
+            console.error("Operations were canceled.");
         } else {
-            console.error("Помилка:", err.message);
+            console.error("Error:", err.message);
         }
     }
 
-    console.log("\n=== Test 3: Обробка помилок ===");
+    console.log("\n=== Test 3: Handling errors ===");
     const numbers3 = [1, "two", 3, 4, "five"];
     try {
         const result3 = await asyncFilter(numbers3, asyncCheckEven, null, 100);
-        console.log("Результат фільтрації:", result3);
+        console.log("Filtering result:", result3);
     } catch (err) {
-        console.error("Помилка:", err.message);
+        console.error("Error:", err.message);
     }
 })();
